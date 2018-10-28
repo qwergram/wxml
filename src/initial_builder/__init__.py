9b934801056a15c1d37011804ff83eae1c32e3f9
qwergram <npengra317@gmail.com>
2018-10-27 21:19:31 -0700

    build more progressbars

--- src/initial_builder/__init__.py
+++
@@ -1,9 +0,0 @@
-# Roses are red
-# Violets are Blue
-# Gerrymandering will someday be dead
-# I'm  B O U N D L E S S  too
-#
-# :)
-# Don't delete this file.
-# No seriously. Don't. You'll break the program.
-# The poem gives the program encouragement to run.
