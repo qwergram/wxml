854f46f74d4a75b9730cf5b2c9d046586c10bb5d
qwergram <npengra317@gmail.com>
2018-11-15 16:29:30 -0800

    werk

--- src/buildmap/__main__.py
+++ src/buildmap/__main__.py
@@ -492,7 +492,7 @@ def drop_nodes(graph, pieces):
             while consuming_node in graph.graph['ghosts']:
                 consuming_node = graph.graph['ghosts'][consuming_node]
 
-            target_consumption_history[drop] = consuming_node
+            graph.graph['ghosts'][drop] = consuming_node
 
             try:
                 drop_node(graph, consuming_node, drop)
