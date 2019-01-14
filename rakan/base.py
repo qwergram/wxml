e2686d8b4f93d5cdee2c3f0ff4e1ac052977373f
qwergram <npengra317@gmail.com>
2019-01-13 17:16:07 -0800

    flesh out baserakan

--- rakan/base.py
+++ rakan/base.py
@@ -2,17 +2,31 @@ from rakan.rakan import PyRakan
 
 import asyncio
 import websockets
+import threading
+
 import json
 import time
 
-BaseRakan = PyRakan
+class BaseRakan(PyRakan):
+    """
+    Basic Rakan format. Use as a template.
+    """
+    iterations = 0 # iterations rakan has gone through
 
-class BaseRakanWithServer(PyRakan):
+    # Scold the user for not implementing anything
+    def step(self, *args, **kwargs):
+        raise NotImplementedError("Not implemented by user!")
+
+    def walk(self, *args, **kwargs):
+        raise NotImplementedError("Not implemented by user!")
+
+class BaseRakanWithServer(BaseRakan):
     """
     Rakan with a websocket for communication with Xayah.
     """
     ws_port = 3001 # websocket port
-    update_speed = 1
+    update_speed = 100000 # number of seconds of how often rakan sends xayah an update
+    iterations = 0 # iterations rakan has gone through
 
     def __init__(self, *args, **kwargs):
         print("Rakan running with websocket server!")
@@ -29,15 +43,19 @@ class BaseRakanWithServer(PyRakan):
     def _create_websocket(self):
         self._init_socket = websockets.serve(self.send_seed, '127.0.0.1', self.ws_port)
         asyncio.get_event_loop().run_until_complete(self._init_socket)
-        asyncio.get_event_loop().run_forever()
+
+        wst = threading.Thread(target=asyncio.get_event_loop().run_forever)
+        wst.daemon = True
+        wst.start()
 
     def send_seed(self, websocket, path):
         print("New Xayah Client!")
         for precinct in self.precincts:
             yield from websocket.send(
                 json.dumps({
-                    "precinct_vertexes": self._vertexes,
-                    "precint_districts": [self.precincts]
+                    "precinct_vertexes": [v[0] for k, v in self._vertexes.items()],
+                    "precint_districts": [_.district for _ in self.precincts],
+                    "iterations": self.iterations,
                 })
             )
         while True:
