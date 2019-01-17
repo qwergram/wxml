5d82381e1a34547d1ba9af9e16d90b3004234a1d
qwergram <npengra317@gmail.com>
2019-01-17 12:40:05 -0800

    debug options to true

--- rakan/setup.py
+++ rakan/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    name='Rakan',
-    ext_modules=cythonize('rakan.pyx', annotate=True),
+    name='Rakan API Toolkit',
+    ext_modules=cythonize('rakan.pyx', annotate=True, gdb_debug=True),
 )
